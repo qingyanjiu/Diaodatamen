78f204d6de635153738bf1297203bc85d30d0f79
Louis <qingyanjiu@126.com>
2019-04-01 23:01:55 +0800

    update

--- src/Charupaixu.java
+++ src/Charupaixu.java
@@ -5,21 +5,21 @@ import java.util.List;
 public class Charupaixu {
 
 	public static void main(String[] args) {
-		int[] numbers = {2,3,5,7,1,4,6,15,5,2,7,9,10,15,9,17,12};
-		List<Integer> result = new ArrayList();
-		result.add(numbers[0]);
+		int[] numbers = {3,2,5,7,1,4,6,15,5,2,7,9,10,15,9,17,12};
+		
 		for(int i=1;i<numbers.length;i++) {
-			result.add(numbers[i]);
-			int len = result.size();
-			for(int j=0;j<len;j++){
-				int last = result.get(len-1);
-				if(last < result.get(j)) {
-					Collections.swap(result, j, len-1);
+			int j = i-1;
+			for(int h = 0;h<=j;h++) {
+				int tmp = numbers[i];
+				if(tmp < numbers[h]) {
+					numbers[i] = numbers[h];
+					numbers[h] = tmp;
 				}
 			}
 		}
-		for(int k = 0,l = result.size();k < l ;k++) {
-			System.out.println(result.get(k));
+		
+		for(int k = 0,l = numbers.length;k < l ;k++) {
+			System.out.println(numbers[k]);
 		}
 	}
 
